e50ff2ea0654579cd9d5ca2a440a32b0302282cc
gui326 <guilherme.326@hotmail.com>
2022-04-10 19:16:53 -0300

    fix error className

--- src/components/Knowledge/index.js
+++ src/components/Knowledge/index.js
@@ -13,64 +13,64 @@ export default function Knowledge(){
                 <Card>
                     <Image src="/images/logoReact.webp" alt="Logo React" width="80" height="80"/>
 
-                    <p class="text--show">React</p>
-                    <p class="info--show">6 meses</p>
+                    <p className="text--show">React</p>
+                    <p className="info--show">6 meses</p>
                 </Card>
 
                 <Card>
                     <Image src="/images/logoNext.png" alt="Logo Next.js" width="120" height="80"/>
 
-                    <p class="text--show">Next.js</p>
-                    <p class="info--show">2 meses</p>
+                    <p className="text--show">Next.js</p>
+                    <p className="info--show">2 meses</p>
                 </Card>
 
                 <Card>
                     <Image src="/images/logoHtml.png" alt="Logo HTML5" width="70" height="80"/>
 
-                    <p class="text--show">HTML5</p>
-                    <p class="info--show">2 anos</p>
+                    <p className="text--show">HTML5</p>
+                    <p className="info--show">2 anos</p>
                 </Card>
 
                 <Card>
                     <Image src="/images/logoCss.png" alt="Logo CSS3" width="80" height="80"/>
 
-                    <p class="text--show">CSS3</p>
-                    <p class="info--show">2 anos</p>
+                    <p className="text--show">CSS3</p>
+                    <p className="info--show">2 anos</p>
                 </Card>
 
                 <Card>
                     <Image src="/images/logoJavascript.png" alt="Logo JavaScript" width="80" height="80"/>
 
-                    <p class="text--show">JavaScript</p>
-                    <p class="info--show">1 ano</p>
+                    <p className="text--show">JavaScript</p>
+                    <p className="info--show">1 ano</p>
                 </Card>
 
                 <Card>
                     <Image src="/images/logoPhp.png" alt="Logo PHP" width="120" height="80"/>
 
-                    <p class="text--show">PHP</p>
-                    <p class="info--show">1 ano</p>
+                    <p className="text--show">PHP</p>
+                    <p className="info--show">1 ano</p>
                 </Card>
 
                 <Card>
                     <Image src="/images/logoLaravel.png" alt="Logo Laravel" width="80" height="80"/>
 
-                    <p class="text--show">Laravel</p>
-                    <p class="info--show">3 meses</p>
+                    <p className="text--show">Laravel</p>
+                    <p className="info--show">3 meses</p>
                 </Card>
 
                 <Card>
                     <Image src="/images/logoMui.png" alt="Logo Material UI" width="90" height="80"/>
 
-                    <p class="text--show">Material UI</p>
-                    <p class="info--show">3 meses</p>
+                    <p className="text--show">Material UI</p>
+                    <p className="info--show">3 meses</p>
                 </Card>
 
                 <Card>
                     <Image src="/images/logoBootstrap.png" alt="Logo Bootstrap" width="100" height="80"/>
 
-                    <p class="text--show">Bootstrap</p>
-                    <p class="info--show">2 anos</p>
+                    <p className="text--show">Bootstrap</p>
+                    <p className="info--show">2 anos</p>
                 </Card>
             </KnowArea>
 
@@ -80,36 +80,36 @@ export default function Knowledge(){
                 <Card>
                     <Image src="/images/logoJava.png" alt="Logo java" width="80" height="80"/>
 
-                    <p class="text--show">Java</p>
-                    <p class="info--show">1 ano</p>
+                    <p className="text--show">Java</p>
+                    <p className="info--show">1 ano</p>
                 </Card>
 
                 <Card>
                     <Image src="/images/logoPython.png" alt="Logo Python" width="80" height="80"/>
 
-                    <p class="text--show">Python</p>
-                    <p class="info--show">4 meses</p>
+                    <p className="text--show">Python</p>
+                    <p className="info--show">4 meses</p>
                 </Card>
 
                 <Card>
                     <Image src="/images/logoFigma.svg" alt="Logo Figma" width="70" height="80"/>
 
-                    <p class="text--show">Figma</p>
-                    <p class="info--show">6 meses</p>
+                    <p className="text--show">Figma</p>
+                    <p className="info--show">6 meses</p>
                 </Card>
 
                 <Card>
                     <Image src="/images/logoGit.png" alt="Logo Git" width="80" height="80"/>
 
-                    <p class="text--show">Git</p>
-                    <p class="info--show">6 meses</p>
+                    <p className="text--show">Git</p>
+                    <p className="info--show">6 meses</p>
                 </Card>
 
                 <Card>
                     <Image src="/images/logoCsharp.webp" alt="Logo C#" width="80" height="80"/>
 
-                    <p class="text--show">C#</p>
-                    <p class="info--show">1 mês</p>
+                    <p className="text--show">C#</p>
+                    <p className="info--show">1 mês</p>
                 </Card>
             </KnowArea>
         </Container>

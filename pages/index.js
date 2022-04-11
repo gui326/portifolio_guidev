c8be11236277af0f799af0fc914a23602bdee933
Gui326 <60348338+gui326@users.noreply.github.com>
2022-04-11 11:41:16 -0300

    fix error tag google analytics

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import HomeScreen from '../src/screens/HomeScreen'
+import Script from 'next/script';
 
 export default function Home() {
   return (
@@ -8,15 +9,17 @@ export default function Home() {
         <title>GuiDev - Portifólio de um desenvolvedor</title>
         <meta name="description" content="GuiDev - Portifólio de um desenvolvedor" />
         <link rel="icon" href="/favicon.ico" />
-        <!-- Global site tag (gtag.js) - Google Analytics -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-T7BGK77Z39"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
+    
+        <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=G-T7BGK77Z39`} />
+        <Script strategy="lazyOnload">
+            {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
 
-          gtag('config', 'G-T7BGK77Z39');
-        </script>
+                gtag('config', 'G-T7BGK77Z39');
+            `}
+        </Script>
       </Head>
 
       <HomeScreen />

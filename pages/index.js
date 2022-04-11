6083d8526008a2d421635090113155c0d191dd53
Gui326 <60348338+gui326@users.noreply.github.com>
2022-04-11 11:34:11 -0300

    add tag google analytics

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,15 @@ export default function Home() {
         <title>GuiDev - Portifólio de um desenvolvedor</title>
         <meta name="description" content="GuiDev - Portifólio de um desenvolvedor" />
         <link rel="icon" href="/favicon.ico" />
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-T7BGK77Z39"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-T7BGK77Z39');
+        </script>
       </Head>
 
       <HomeScreen />

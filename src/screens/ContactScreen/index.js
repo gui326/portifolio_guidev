e004fcd4a2308fed0eb2ab3eecf84736b2a44774
gui326 <guilherme.326@hotmail.com>
2022-04-16 19:57:46 -0300

    inserido contatos

--- src/screens/ContactScreen/index.js
+++ src/screens/ContactScreen/index.js
@@ -1,8 +1,12 @@
 import { Container } from "@mui/material";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
+import EmailIcon from '@mui/icons-material/Email';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import Link from "next/link";
 
-import { Title, Description } from "./styled";
+import { Title, Description, Card } from "./styled";
 
 
 export default function ContactScreen(){
@@ -17,6 +21,35 @@ export default function ContactScreen(){
                 <Description>
                     entre em <span>contato</span> para a gente trocar uma ideia
                 </Description>
+
+                <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                    <Link href="mailto:guilherme.326@hotmail.com" passHref>
+                        <a target="_blank">
+                            <Card>
+                                <EmailIcon />
+                                <p>guilherme.326@hotmail.com</p>
+                            </Card>
+                        </a>
+                    </Link>
+
+                    <Link href="https://www.linkedin.com/in/guilherme-batista-b38400197/" passHref>
+                        <a target="_blank">
+                            <Card>
+                                <LinkedInIcon />
+                                <p>Acessar LinkedIn</p>
+                            </Card>
+                        </a>
+                    </Link>
+
+                    <Link href="https://github.com/gui326" passHref>
+                        <a target="_blank">
+                            <Card>
+                                <GitHubIcon />
+                                <p>Acessar Github</p>
+                            </Card>
+                        </a>
+                    </Link>
+                </div>
             </Container>
 
             <Footer />
--- src/screens/ContactScreen/styled.js
+++ src/screens/ContactScreen/styled.js
@@ -26,3 +26,35 @@ export const Description = styled.p`
         -webkit-text-fill-color: transparent;
     }
 `;
+
+export const Card = styled.div` 
+    border: 2px solid black;
+    display: flex;
+    flex-direction: row;
+    align-self: center;
+    align-items: center;
+    margin-bottom: 1em;
+    padding: .2em 1.3em;
+    width: 350px;
+    cursor: pointer;
+    transition: border .3s ease-out;
+
+    p {
+        margin-left: 1em;
+    }
+
+    :hover{
+        border-image-source: linear-gradient(to left, #670b62, #ce213d);
+        border-image-slice: 1;
+        
+        p{
+            background: -webkit-linear-gradient(left,#670b62 10%, #a51d62 30%, #ce213d 80%);
+            -webkit-background-clip: text;
+            -webkit-text-fill-color: transparent;
+        }
+    }
+
+    :active{
+        outline: 5px solid lightgrey;
+    }
+`;
